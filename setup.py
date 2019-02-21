cb1f02ed5a68806af7a653ece52c31d33c5060f1
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2019-02-21 01:12:55 -0800

    Fixed gym bug

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='baxter_gym',
       version='0.0.1',
-      install_requires=['dm_control', 'gym', 'numpy', 'matplotlib', 'pybullet']
+      install_requires=['dm_control', 'gym==0.10.9', 'numpy', 'matplotlib', 'pybullet']
 )  
