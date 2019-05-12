ed4fbf42e7d4c3cc7b896405dfde82a52eceea1f
michaelmcdonald <m_j_mcdonald@berkeley.edu>
2019-05-11 22:14:20 -0700

    New env

--- baxter_gym/__init__.py
+++ baxter_gym/__init__.py
@@ -13,3 +13,15 @@ register(
     max_episode_steps=250,
     reward_threshold=2500,
 )
+
+register(
+    id='BaxterBlockStack-v0',
+    entry_point='baxter_gym.envs:BaxterBlockStackEnv',
+    max_episode_steps=20,
+)
+
+register(
+    id='BaxterLeftBlockStack-v0',
+    entry_point='baxter_gym.envs:BaxterLeftBlockStackEnv',
+    max_episode_steps=20,
+)
